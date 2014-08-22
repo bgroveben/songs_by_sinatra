a6a113ea4d153ec97989578cea2074a9394d350d
bgrove1974 <bgrove1974@gmail.com>
2014-08-21 20:59:41 -0700

    Set page titles

--- main.rb
+++ main.rb
@@ -2,17 +2,21 @@ require 'sinatra'
 require 'sinatra/reloader' if development?
 
 get '/' do
+  @title = "Welcome to the HomePage"
   erb :home
 end
 
 get '/about' do
+  @title = "All About This Website"
   erb :about
 end
 
 get '/contact' do
+  @title = "Contact Information"
   erb :contact
 end
 
 not_found do
+  @title = "Oops, Not Found"
   erb :not_found
 end
