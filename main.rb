01c0d4e274e0661fc61ee0afc1408f8dad1d4c9e
bgrove1974 <bgrove1974@gmail.com>
2014-09-04 00:28:44 -0700

    Move application routes into a modular structure

--- main.rb
+++ main.rb
@@ -1,44 +1,45 @@
-require 'sinatra'
-require 'sinatra/reloader' if development?
-require 'sinatra/flash'
+require 'sinatra/base'
 require 'slim'
 require 'sass'
-require 'data_mapper'
+require 'sinatra/flash'
 require 'pony'
-require './song'
 require './sinatra/auth'
 require 'v8'
 require 'coffee-script'
 
-configure do
-  enable :sessions
-  set :username, 'frank'
-  set :password, 'sinatra'
-end
+class Website < Sinatra::Base
+  register Sinatra::Auth
+  register Sinatra::Flash
 
-configure :development do
-  DataMapper.setup(:default, "sqlite3://#{Dir.pwd}/development.db")
-##Code for email setup
-#  set :email_address => 'smtp.gmail.com',
-#    :email_user_name => 'frank',
-#    :email_password => 'sinatra',
-#    :email_domain => 'localhost.localdomain'
-end
+  configure do
+    enable :sessions
+    set :username, 'frank'
+    set :password, 'sinatra'
+  end
 
-configure :production do
-  DataMapper.setup(:default, ENV['DATABASE_URL'])
-##Code for email setup
-#  set :email_address => 'smtp.sendgrid.net',
-#  :email_user_name => ENV['SENDGRID_USERNAME'],
-#  :email_password => ENV['SENDGRID_PASSWORD'],
-#  :email_domain => 'heroku.com'
-end
+  configure :development do
+    # DataMapper.setup(:default, "sqlite3://#{Dir.pwd}/development.db")
+  ##Code for email setup
+  #  set :email_address => 'smtp.gmail.com',
+  #    :email_user_name => 'frank',
+  #    :email_password => 'sinatra',
+  #    :email_domain => 'localhost.localdomain'
+  end
 
-before do
-  set_title
-end
+  configure :production do
+    # DataMapper.setup(:default, ENV['DATABASE_URL'])
+  ##Code for email setup
+  #  set :email_address => 'smtp.sendgrid.net',
+  #  :email_user_name => ENV['SENDGRID_USERNAME'],
+  #  :email_password => ENV['SENDGRID_PASSWORD'],
+  #  :email_domain => 'heroku.com'
+  end
 
-helpers do
+  before do
+    set_title
+  end
+
+  # helpers do
   def css(*stylesheets)
     stylesheets.map do |stylesheet|
       "<link href=\"/#{stylesheet}.css\" media=\"screen, projection\" rel=\"stylesheet\" />"
@@ -73,57 +74,54 @@ helpers do
       }
     )
   end
-end
 
-##Instead of route handlers below, use /sinatra/auth.rb
-#get '/login' do
-#  slim :login
-#end
 
-#post '/login' do
-#  if params[:username] == settings.username && params[:password] == settings.password
-#    session[:admin] = true
-#    redirect to('/songs')
-#  else
-#    slim :login
-#  end
-#end
+  ##Instead of route handlers below, use /sinatra/auth.rb
+  #get '/login' do
+  #  slim :login
+  #end
 
-#get '/set/:name' do
-#  session[:name] = params[:name]
-#end
+  #post '/login' do
+  #  if params[:username] == settings.username && params[:password] == settings.password
+  #    session[:admin] = true
+  #    redirect to('/songs')
+  #  else
+  #    slim :login
+  #  end
+  #end
 
-#get '/logout' do
-#  session.clear
-#  redirect to('/login')
-#end
+  #get '/set/:name' do
+  #  session[:name] = params[:name]
+  #end
 
-get '/get/hello' do
-  "Hello #{session[:name]}"
-end
+  #get '/logout' do
+  #  session.clear
+  #  redirect to('/login')
+  #end
 
-get('/styles.css'){ scss :styles }
-get('/javascripts/application.js'){ coffee :application }
+  get('/styles.css'){ scss :styles }
+  get('/javascripts/application.js'){ coffee :application }
 
-get '/' do
-  slim :home
-end
+  get '/' do
+    slim :home
+  end
 
-get '/about' do
-  @title = "All About This Website"
-  slim :about
-end
+  get '/about' do
+    @title = "All About This Website"
+    slim :about
+  end
 
-get '/contact' do
-  slim :contact
-end
+  get '/contact' do
+    slim :contact
+  end
 
-post '/contact' do
-  send_message
-  flash[:notice] = "Thanks for your message. We'll get back to you soon!"
-  redirect to('/')
-end
+  post '/contact' do
+    send_message
+    flash[:notice] = "Thanks for your message. We'll get back to you soon!"
+    redirect to('/')
+  end
 
-not_found do
-  slim :not_found
+  not_found do
+    slim :not_found
+  end
 end
--- song.rb
+++ song.rb
@@ -1,5 +1,10 @@
+require 'sinatra/base'
 require 'dm-core'
 require 'dm-migrations'
+require 'slim'
+require 'sass'
+require 'sinatra/flash'
+require './sinatra/auth'
 
 class Song
   include DataMapper::Resource
@@ -37,31 +42,68 @@ module SongHelpers
   end
 end
 
+class SongController < Sinatra::Base
+  enable :method_override
+  register Sinatra::Flash
+  register Sinatra::Auth
+
 helpers SongHelpers
 
-get '/songs' do
+configure do
+  enable :sessions
+  set :username, 'frank'
+  set :password, 'sinatra'
+end
+
+configure :development do
+  DataMapper.setup(:default, "sqlite3://#{Dir.pwd}/development.db")
+end
+
+configure :production do
+  DataMapper.setup(:default, ENV['DATABASE_URL'])
+end
+
+before do
+  set_title
+end
+
+def css(*stylesheets)
+  stylesheets.map do |stylesheet|
+    "<link href=\"/#{stylesheet}.css\" media=\"screen, projection\" rel=\"stylesheet\" />"
+  end.join
+end
+
+def current?(path='/')
+  (request.path==path || request.path==path+'/') ? "current" : nil
+end
+
+def set_title
+  @title ||= "Songs By Sinatra"
+end
+
+get '/' do
   find_songs
   slim :songs
 end
 
-get '/songs/new' do
+get '/new' do
   protected!
   @song = Song.new
   slim :new_song
 end
 
-get '/songs/:id' do
+get '/:id' do
   @song = find_song
   slim :show_song
 end
 
-get '/songs/:id/edit' do
+get '/:id/edit' do
   protected!
   @song = find_song
   slim :edit_song
 end
 
-post '/songs' do
+post '/' do
   protected!
   if create_song
     flash[:notice] = "Song successfully added"
@@ -69,7 +111,7 @@ post '/songs' do
   redirect to("/songs/#{@song.id}")
 end
 
-put '/songs/:id' do
+put '/:id' do
   protected!
   song = find_song
   if song.update(params[:song])
@@ -78,7 +120,7 @@ put '/songs/:id' do
   redirect to("/songs/#{song.id}")
 end
 
-delete '/songs/:id' do
+delete '/:id' do
   protected!
   if find_song.destroy
     flash[:notice] = "Song deleted"
@@ -86,7 +128,7 @@ delete '/songs/:id' do
   redirect to('/songs')
 end
 
-post '/songs/:id/like' do
+post '/:id/like' do
   @song = find_song
   @song.likes = @song.likes.next
   @song.save
